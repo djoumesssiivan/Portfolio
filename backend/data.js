e2c8c4ac39d1449bd42c5c25c0132797873d3806
djoumessiivan <ivandjoumessi2@gmail.com>
2023-06-21 10:22:59 +0100

    update

--- backend/data.js
+++ backend/data.js
@@ -1,9 +1,19 @@
 const TESTIMONIAL =[
+{
+    "Clientname" : "Donbou Delano",
+    "testimonialMessage" : "he is amazing! , i am very happy to know someone like her because she is hard working and i like it.",
+    "clientProfile" : "https://firebasestorage.googleapis.com/v0/b/djoumes-portfolio.appspot.com/o/ani7.jpg?alt=media&token=d92a11cc-cd0c-465b-948a-ac0ba7df9c92"
+  }  ,
   {
-    "Clientname" : "Djoumes portfolio",
-    "TestimonialMessage" : "he is amazing! , i am very happy to know someone like her because she is hard working and i like it.",
-    "ClientImage" : "https://firebasestorage.googleapis.com/v0/b/djoumes-portfolio.appspot.com/o/ani7.jpg?alt=media&token=d92a11cc-cd0c-465b-948a-ac0ba7df9c92"
-  },
+    "clientname" : "Kenne Suzy",
+    "testimonialMessage" : "the best developer which is hire for my project and the team was working so hard on my project all the wok will posted on a perfect time.",
+    "clientProfile" : "https://firebasestorage.googleapis.com/v0/b/djoumes-portfolio.appspot.com/o/ani3.jpg?alt=media&token=0225dc12-6ba1-45ef-adf0-a86029e13bc1",
+},
+{
+  "clientname" : "lekane wendy",
+  "testimonialMessage" : "Djoumes is a friend and a very fantastic person for everyone in are surroundings. she is a fun girl who exudes good humor around her. she is a computer engineering student and future engineer who is doing extraordinary things that will revolutionize our lives. she is very amazing. and as we often say the key to everything is work so I'm sure you'll achieve a lot. you're someone very special.",
+  "clientProfilee" : "https://firebasestorage.googleapis.com/v0/b/djoumes-portfolio.appspot.com/o/ani5.jpg?alt=media&token=e9dd2a9e-eb2b-4e95-b797-361c8cbcae86",
+}
 
   // {
   //   "Clientname" : "No name",
@@ -16,14 +26,52 @@ const PROJECT = [
   {
   "images":"https://firebasestorage.googleapis.com/v0/b/djoumes-portfolio.appspot.com/o/moi1.jpg?alt=media&token=ede3763a-5a8c-4804-86a4-f6d01676b1d6",
   "Githublink":"https://github.com/djoumessiivan/portfolio.git",
- // "liveLink":
   "title" :"Project 1"
   },
-  // {
-  //   "images":
-  //   "Livelink":
-  //   "title" :
-  //   "Githublink":
-  // },
+  
+ 
+]
+
+
+const EXPERIENCE = [
+  {
+      "name" : "FRONTEND DEVELOPMENT",
+      "experience" : [
+        {
+          "experienceName" : "HTML",
+          "experienceLevel" : "Experienced",
+        },
+        {
+                 "experienceName" : "CSS",
+          "experienceLevel" : "Experienced",	
+        },
+        {
+                "experienceName" : "JavaScript",
+          "experienceLevel" : "Experienced",
+        },
+        {
+          "experienceName" : "React",
+          "experienceLevel" : "Intermediate"
+        }
+      ]
+  },
+  {
+      "name" : "BACKEND DEVELOPMENT",
+      "experience" : [
+        {
+          "ExperienceName" : "Node JS",
+          "experienceLevel" : "Beginning",
+        },
+        {
+                "ExperienceName" : "Express",
+          "experienceLevel" : "Beginning",
+        },
+        {
+          "ExperienceName" : "MongoDB",
+          "experienceLevel" : "Beginning",
+        }
+      
+      ]
+  }
  
 ]
